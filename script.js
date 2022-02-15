ea274830103d44992e6fc3fe80d005fadef0067a
Wendell Rios <werios@deloitte.com>
2022-02-15 11:47:47 -0300

    cria idSalesforce aleatória apenas caso não exista

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-localStorage.setItem("idSalesforce", "aA3Asff1gm34");
+function getRndInteger(min, max) {
+    numero = Math.floor(Math.random() * (max - min)) + min;
+    return numero;
+  }
+// idSalesforce = "aA3Asff1gm34";
+
+if(!localStorage.getItem("idSalesforce")){
+  console.log("Hello! Setando idSalesforce")
+  idSalesforce = "Chave" + getRndInteger(10000000,99999999);
+  localStorage.setItem("idSalesforce",  idSalesforce);
+}
