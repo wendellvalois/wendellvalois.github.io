96e9a240e1a920835a6cccb8ef15fc559e737492
Wendell Rios <werios@deloitte.com>
2022-03-28 15:52:24 -0300

    Testes GA

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function getRndInteger(min, max) {
-    numero = Math.floor(Math.random() * (max - min)) + min;
-    return numero;
-  }
+  numero = Math.floor(Math.random() * (max - min)) + min;
+  return numero;
+}
 // idSalesforce = "aA3Asff1gm34";
 
-if(!localStorage.getItem("idSalesforce")){
-  console.log("Hello! Setando idSalesforce")
-  idSalesforce = "Chave" + getRndInteger(10000000,99999999);
-  localStorage.setItem("idSalesforce",  idSalesforce);
+if (!localStorage.getItem("idSalesforce")) {
+  console.log("Hello! Setando idSalesforce");
+  idSalesforce = "Chave" + getRndInteger(10000000, 99999999);
+  localStorage.setItem("idSalesforce", idSalesforce);
+  console.log(idSalesforce);
 }
